fc01c1e529be647b9f0666e2b507e6ff610e949f
Giulliany Lima Bezerra <53629399+Giulliany@users.noreply.github.com>
2020-03-24 16:35:50 -0300

    segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CadastroPessoasTotalizador';
+  nome = null;
+  pessoas = [];
+  totalizador = null;
+
+  adicionar(){
+    this.pessoas.push(this.nome);
+    this.nome = null;
+    this.totalizador += 1;
+  }
+
+  excluir(id){
+    this.pessoas.splice(this.pessoas.indexOf(id), 1);
+    this.totalizador -=1; 
+  }
 }
